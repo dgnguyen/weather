0562d9a1dda5a7ad9b41fd290a3c6198693fad5a
dgnguyen <ducgiang1206@gmail.com>
2020-10-05 22:28:29 +0200

    68 use weatherstack instead of darksky

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+    const url = 'http://api.weatherstack.com/current?access_key=6c7b98d2aeda984e3a3edebaa2f3c153&query=' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
+        
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            const {location:{name},current:{weather_descriptions,temperature,humidity}} = body
+            callback(undefined,weather_descriptions.join(), '. It is currently ' + temperature + ' degress out. Humidity is ' + humidity)
         }
     })
 }
